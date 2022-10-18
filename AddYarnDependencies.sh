7ad7dd05ae9de8c764c1f785a94a5bd355ae8d60
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-10-18 09:45:35 +0200

    Bump versions

--- AddYarnDependencies.sh
+++ AddYarnDependencies.sh
@@ -1,5 +1,5 @@
 cd MyApp && \
-yarn add react-native-reanimated@3.0.0-rc.3 react-native-gesture-handler@2.6.2 react-native-screens@3.17.0 react-native-svg@13.2.0 react-native-safe-area-context@4.3.4 && \
+yarn add react-native-reanimated@3.0.0-rc.3 react-native-gesture-handler@2.7.1 react-native-screens@3.18.2 react-native-svg@13.4.0 react-native-safe-area-context@4.4.1 && \
 yarn add --dev typescript "@tsconfig/react-native" "@types/jest" "@types/react-native" "@types/react-test-renderer" "@typescript-eslint/eslint-plugin" "@typescript-eslint/parser" && \
 yarn add --dev "@babel/preset-env" patch-package postinstall-postinstall && \
 yarn add appium webdriverio
