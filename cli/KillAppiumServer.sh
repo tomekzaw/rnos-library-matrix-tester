05dae3254613c4512a34cb30c46b8d3235d9e6ca
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-01-24 21:55:41 +0100

    Remove unnecessary `export`

--- cli/KillAppiumServer.sh
+++ cli/KillAppiumServer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-export APPIUM_SERVER_PID=$(lsof -t -i tcp:4723)
+APPIUM_SERVER_PID=$(lsof -t -i tcp:4723)
 
 if [ -n "$APPIUM_SERVER_PID" ]; then
     kill -9 $APPIUM_SERVER_PID
--- cli/KillMetroBundler.sh
+++ cli/KillMetroBundler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-export METRO_BUNDLER_PID=$(lsof -t -i tcp:8081)
+METRO_BUNDLER_PID=$(lsof -t -i tcp:8081)
 
 if [ -n "$METRO_BUNDLER_PID" ]; then
     kill -9 $METRO_BUNDLER_PID
