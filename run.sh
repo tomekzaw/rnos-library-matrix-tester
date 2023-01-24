dfacf6526f75184b22c7db0899fb75349a88650e
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-01-24 11:59:30 +0100

    Update ios.yml

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ export E2E_MODE=debug
 export E2E_PLATFORM=iOS
 
 # app and simulator config, please don't change unless something doesn't work
-export E2E_APP_NAME=MyApp
 export E2E_IOS_SIMULATOR_NAME="iPhone 14 Pro"
 export E2E_IOS_SIMULATOR_VERSION=16.2
+export E2E_APP_NAME=MyApp
 
 # derived values, please don't edit
 export E2E_APP_PATH=$SCRIPT_DIR/$E2E_APP_NAME
