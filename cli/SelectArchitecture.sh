a18a5acf2fbd915bb4bc2e2503c5fb921d5964d6
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-01-25 08:50:56 +0100

    Fix paths

--- cli/SelectArchitecture.sh
+++ cli/SelectArchitecture.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -u
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 : "${E2E_ARCHITECTURE}" # bound variable
 
-node SelectArchitecture.js
+node $SCRIPT_DIR/SelectArchitecture.js
