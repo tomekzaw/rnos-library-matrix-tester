3d23325072cd7b6cbc5d97b58d9f09c5783ef07e
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-01-30 13:02:11 +0100

    Fix enabling Hermes on iOS

--- cli/SelectRuntime.js
+++ cli/SelectRuntime.js
@@ -6,14 +6,17 @@ const REACT_NATIVE_MINOR_VERSION = Number(
   process.env.E2E_REACT_NATIVE_VERSION.split(".")[1]
 );
 
-const path =
-  REACT_NATIVE_MINOR_VERSION >= 71
-    ? process.env.E2E_APP_PATH + "/android/gradle.properties"
-    : process.env.E2E_APP_PATH + "/android/app/build.gradle";
+const path = process.env.E2E_PLATFORM === "iOS"
+  ? "/ios/Podfile"
+  : REACT_NATIVE_MINOR_VERSION >= 71
+  ? process.env.E2E_APP_PATH + "/android/gradle.properties"
+  : process.env.E2E_APP_PATH + "/android/app/build.gradle";
 
 let text = fs.readFileSync(path, "utf-8");
 
-if (REACT_NATIVE_MINOR_VERSION >= 71) {
+if (process.env.E2E_PLATFORM === "iOS") {
+  text = text.replace(/:hermesEnabled => (.*)/, ":hermesEnabled => " + value);
+} else if (REACT_NATIVE_MINOR_VERSION >= 71) {
   text = text.replace(/hermesEnabled=(false|true)/, "hermesEnabled=" + value);
 } else {
   text = text.replace(/enableHermes: (false|true)/, "enableHermes: " + value);
